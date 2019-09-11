9893c7bfaa550c4ba65413c114683bcea1e05cb3
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-09-10 22:38:56 -0400

    Copied design from rigel.

---
+++ design/src/org/nasdanika/rigel/design/Services.java
@@ -0,0 +1,21 @@
+package org.nasdanika.rigel.design;
+
+import java.util.Date;
+
+import org.eclipse.emf.ecore.EObject;
+import org.nasdanika.rigel.Milestone;
+
+/**
+ * The services class used by VSM.
+ */
+public class Services {
+    
+    /**
+    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
+    */
+    public EObject myService(EObject self, String arg) {
+       // TODO Auto-generated code
+      return self;
+    }
+    
+}
