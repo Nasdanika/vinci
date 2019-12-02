e427ccb356c92cfeb3b30426ba1e91cd535c7f02
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-12-02 02:36:31 -0500

    Selects are working.

--- design/src/org/nasdanika/vinci/design/Services.java
+++ design/src/org/nasdanika/vinci/design/Services.java
@@ -1,30 +1,22 @@
 package org.nasdanika.vinci.design;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.sirius.properties.EditSupport;
 
 /**
  * The services class used by VSM.
  */
 public class Services {
-
-	/**
-	 * See
-	 * http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24
-	 * for documentation on how to write service methods.
-	 */
-	public EObject myService(EObject self, String arg) {
-		System.out.println(arg);
-		// TODO Auto-generated code
-		return self;
-	}
-
-	/**
-    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
-    */
-    public boolean hasChoicess(EObject self, Object editService, Object ref) {
-    	System.out.println(editService);
-    	System.out.println(ref);
-    	return true;
+    
+    public boolean isSelect(EObject self, EditSupport editSupport, EStructuralFeature feature) {
+    	try {
+    		return editSupport.getChoiceOfValues(feature) != null;
+    	} catch (NullPointerException e) {
+    		// Edit support throws an exception if choices are null.
+    		return false;
+    	}
     }
+    
 
 }
