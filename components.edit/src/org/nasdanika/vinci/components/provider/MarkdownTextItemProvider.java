e8ec22df903114db7c60a117846724ae8b8aca22
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-12-02 00:23:59 -0500

    Rusification test.

--- components.edit/src/org/nasdanika/vinci/components/provider/MarkdownTextItemProvider.java
+++ components.edit/src/org/nasdanika/vinci/components/provider/MarkdownTextItemProvider.java
@@ -8,12 +8,9 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
-import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
-
 import org.nasdanika.vinci.components.ComponentsPackage;
 import org.nasdanika.vinci.components.Markdown;
 import org.nasdanika.vinci.components.MarkdownText;
