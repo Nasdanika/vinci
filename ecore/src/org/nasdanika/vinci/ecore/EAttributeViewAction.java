ecec8f3272b5fe076dfe0246a6778761b258bc5e
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-03-25 12:08:02 -0400

    Copied emf and ecore from HTML. Before cleanup and refactoring.

---
+++ ecore/src/org/nasdanika/vinci/ecore/EAttributeViewAction.java
@@ -0,0 +1,12 @@
+package org.nasdanika.html.ecore;
+
+import org.eclipse.emf.ecore.EAttribute;
+
+public class EAttributeViewAction extends EStructuralFeatureViewAction<EAttribute> {
+
+	public EAttributeViewAction(EAttribute value) {
+		super(value);
+	}
+		
+
+}
