b4574f4e5c90e7c3db78d54582b301a42e4078d0
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-04-02 11:32:26 -0400

    Typed element properties.

--- ecore/src/org/nasdanika/vinci/ecore/ETypedElementViewActionSupplier.java
+++ ecore/src/org/nasdanika/vinci/ecore/ETypedElementViewActionSupplier.java
@@ -6,7 +6,6 @@ import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.common.SupplierFactory;
 import org.nasdanika.ncore.NcoreFactory;
 import org.nasdanika.ncore.Property;
-import org.nasdanika.ncore.Value;
 import org.nasdanika.vinci.bootstrap.Appearance;
 import org.nasdanika.vinci.bootstrap.BootstrapFactory;
 import org.nasdanika.vinci.bootstrap.Table;
@@ -35,38 +34,10 @@ public class ETypedElementViewActionSupplier<T extends ETypedElement> extends EN
 		}
 		
 		action.getContent().add((SupplierFactory) propertiesTable(monitor)); 
-
-//		String name = super.getText();
-//		EObject container = target.eContainer();
-//		if (container instanceof EClassifier) {
-//			EClassifier classifier = (EClassifier) container;
-//			label.append(classifier.getName()); /// ???? Why ????
-//		} else {
-//			label.append(name);
-//		}
 		action.setText(label.toString());
 	}
-	
-//	@Override
-//	public Object generate(ViewGenerator viewGenerator, ProgressMonitor progressMonitor) {
-//		BootstrapFactory bootstrapFactory = viewGenerator.get(BootstrapFactory.class);
-//		Container contentContainer = bootstrapFactory.fluidContainer();
-//		contentContainer.text().alignment(Alignment.LEFT);
-//		Table propertiesTable = propertiesTable(viewGenerator);
-//		if (!propertiesTable.toHTMLElement().isEmpty()) {
-//			contentContainer.row().col(propertiesTable)
-//				.width(Breakpoint.DEFAULT, Size.NONE)
-//				.padding().bottom(Breakpoint.DEFAULT, Size.S3);
-//		}
-//		String description = getDescription();
-//		if (!Util.isBlank(description)) {
-//			contentContainer.row().col(description).width(Breakpoint.DEFAULT, Size.NONE);
-//		}		
-//		
-//		return contentContainer;
-//	}
-//	
-	protected Table propertiesTable(ProgressMonitor monitor) {		
+
+	protected Table propertiesTable(ProgressMonitor monitor) throws Exception {		
 		Table table = BootstrapFactory.eINSTANCE.createTable();
 		
 		Appearance tableAppearance = BootstrapFactory.eINSTANCE.createAppearance();
@@ -76,13 +47,19 @@ public class ETypedElementViewActionSupplier<T extends ETypedElement> extends EN
 		styleAttribute.setValue("width:auto");
 		tableAppearance.getAttributes().add(styleAttribute);
 		
-		EGenericType genericType = eObject.getEGenericType(); // TODO - generic type
+		EGenericType genericType = eObject.getEGenericType(); 
 		if (genericType != null) {
-//			// TODO - generic parameters with links.
-//			ViewAction typeViewAction = EObjectAdaptable.adaptTo(type, ViewAction.class);
-//			Row typeRow = table.row();
-//			typeRow.header(getResourceContext().getString(PropertyKeys.UI_TYPE, "Type"));
-//			typeRow.cell(typeViewAction == null ?  type.getName() : viewGenerator.link(typeViewAction));
+			TableRow typeRow = BootstrapFactory.eINSTANCE.createTableRow();
+			table.getRows().add(typeRow);
+			
+			TableCell typeRowHeader = BootstrapFactory.eINSTANCE.createTableCell();
+			typeRow.getCells().add(typeRowHeader);
+			typeRowHeader.setHeader(true);
+			typeRowHeader.getContent().add(wrap("Type"));
+			
+			TableCell typeRowCell = BootstrapFactory.eINSTANCE.createTableCell();
+			typeRow.getCells().add(typeRowCell);
+			genericType(genericType, typeRowCell.getContent(), monitor);
 		}
 		
 		TableRow cardinalityRow = BootstrapFactory.eINSTANCE.createTableRow();
@@ -91,15 +68,11 @@ public class ETypedElementViewActionSupplier<T extends ETypedElement> extends EN
 		TableCell cardinalityRowHeader = BootstrapFactory.eINSTANCE.createTableCell();
 		cardinalityRow.getCells().add(cardinalityRowHeader);
 		cardinalityRowHeader.setHeader(true);
-		Value cardinalityHeaderValue = NcoreFactory.eINSTANCE.createValue();
-		cardinalityHeaderValue.setValue("Cardinality");
-		cardinalityRowHeader.getContent().add(cardinalityHeaderValue);
+		cardinalityRowHeader.getContent().add(wrap("Cardinality"));
 		
 		TableCell cardinalityRowCell = BootstrapFactory.eINSTANCE.createTableCell();
 		cardinalityRow.getCells().add(cardinalityRowCell);
-		Value cardinalityCellValue = NcoreFactory.eINSTANCE.createValue();
-		cardinalityCellValue.setValue(cardinality(eObject));
-		cardinalityRowCell.getContent().add(cardinalityCellValue);
+		cardinalityRowCell.getContent().add(wrap(cardinality(eObject)));
 		
 		return table;
 	}
