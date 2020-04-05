7bbead4bf73c16f42a1468e054f12535aa15e435
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-04-05 10:53:11 -0400

    NPE fix.

--- ecore/src/org/nasdanika/vinci/ecore/EParameterViewActionSupplier.java
+++ ecore/src/org/nasdanika/vinci/ecore/EParameterViewActionSupplier.java
@@ -1,13 +1,21 @@
 package org.nasdanika.vinci.ecore;
 
 import org.eclipse.emf.ecore.EParameter;
+import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.vinci.app.Action;
 import org.nasdanika.vinci.app.ActionRole;
 
 public class EParameterViewActionSupplier extends ETypedElementViewActionSupplier<EParameter> {
 
 	public EParameterViewActionSupplier(EParameter value) {
 		super(value);
+	}
+	
+	@Override
+	protected Action create(ProgressMonitor progressMonitor) throws Exception {
+		Action ret = super.create(progressMonitor);
 		action.setRole(ActionRole.SECTION.label);
+		return ret;
 	}
 
 }
